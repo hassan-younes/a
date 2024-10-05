d0e95d18c9b2d2a0f0615ab2512e3450d150ac30
hassan-younes <hassanyounes783@gmail.com>
2024-10-05 22:18:47 +0300

    account page/nestedRoutes

--- app/_components/Navigation.jsx
+++ app/_components/Navigation.jsx
@@ -1,25 +1,40 @@
+'use client'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function Navigation() {
+  const navLinks = [
+    {
+      name: "Cabins",
+      href: "/cabins",
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+    {
+      name: "Guest area",
+      href: "/account",
+     
+    },
+  ];
+  const pathname=usePathname()
+  
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
-        <li>
-          <a href="/cabins" className="hover:text-accent-400 transition-colors">
-            Cabins
-          </a>
-        </li>
-        <li>
-          <a href="/about" className="hover:text-accent-400 transition-colors">
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </a>
-        </li>
+      {navLinks.map((link)=>(
+        
+        <li key={link.name}>
+        <Link
+         href={link.href}
+           className={`hover:text-accent-400 ${pathname.split("/")[1]===link.href.split("/")[1]?"text-accent-400":""}  transition-colors`}
+        >
+          
+          {link.name}
+        </Link>
+      </li>
+        ))}
+       
       </ul>
     </nav>
   );
